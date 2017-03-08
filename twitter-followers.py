c92cf1af72b475eae85db0c640c2bdc1f90b0d60
Wesley Archer <wesleyarcher@hotmail.com>
2017-03-08 19:23:19 +0000

    Updated code
    Tidied the code to make it easier to understand

--- twitter-followers.py
+++ twitter-followers.py
@@ -4,46 +4,36 @@ import tweepy
 import urllib2
 import time
 
-# The time (in seconds) between checks. Default is 1800 seconds (30 minutes)
-wait = 1800
-
-# Replace with your incoming webhook URL generated in Slack
-webhook = ''
-
-# Colour for "OK" message
-ok = "#74a727"
-
-# Colour for "Uh oh!" message
-warn = "#971a1a"
-
-# Add the relevant keys, tokens and secrets from your Twitter app made here: https://apps.twitter.com/
+# For Twitter: Add the relevant keys, tokens and secrets from your Twitter app made here: https://apps.twitter.com/
 
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
 
-# Define the user you want to track the followers of below (without the @)
-user = 'raspberrycoulis'
+# Variables - configure the bits below to get your script working. 
 
-# Tweepy API variables and stuff
+wait = 3600         # Time (in seconds) between checks. Default is 3600 seconds (60 minutes)
+style = "#74a727"   # Colour for the message - this is the green from the Raspberry Pi logo
+userid = ''           # The Twitter user you want to track the followers of (without the @)
+handle = ''         # Tweak this to display the userid in a nicer format - i.e. "Raspberry Coulis" instead of "raspberrycoulis"
+
+# Slack incoming webhook URL
+
+webhook = ''
+
+# Tweepy API - do not change
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
-# Defines the api variable for convenience
-
 api = tweepy.API(auth)
+follows = api.get_user(userid)
 
-# Tells the Tweepy API to get the user details specified earlier
-
-follows = api.get_user(user)
-
-# Experimenting with a function
+# The function that does the magic - checks your Twitter user for the number of followers then sends this data to Slack to notify you.
 
 def followers():
   while True:
-    look = str(follows.followers_count)
-    data = '{"attachments":[{"fallback":"'+user+' has '+look+' followers.","pretext":"'+user+' has '+look+' followers.","color":"'+ok+'","fields":[{"title":"OK","value":"'+user+' has '+look+' followers.","short":false}]}]}'
+    fans = str(follows.followers_count)
+    data = '{"attachments":[{"fallback":"'+handle+' has '+fans+' followers.","pretext":"'+handle+' has '+fans+' followers.","color":"'+style+'","fields":[{"title":"Followers","value":"'+handle+' has '+fans+' followers.","short":false}]}]}'
     slack = urllib2.Request(webhook, data, {'Content-Type': 'application/json'})
     f = urllib2.urlopen(slack)
     f.close()
